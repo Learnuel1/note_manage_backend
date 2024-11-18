623d1f77eadaa6a3c68aff670d2ca5b3885fc707
Linus Learnuel <63509139+Learnuel1@users.noreply.github.com>
2024-11-18 17:01:59 +0100

    feat: implemented authentication

--- auth.controller.js
+++ auth.controller.js
@@ -1,7 +1,8 @@
 const { compareSync } = require("bcryptjs");
 const AccountModel = require("./Account.model");
 const { APIError } = require("./models/apiError");
-
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 exports.login = async (req, res, next) => {
   try {
     const {email, password} = req.body;
@@ -9,9 +10,18 @@ exports.login = async (req, res, next) => {
     if(!password) return next(APIError.badRequest("Password is required"));
     const userExist = await AccountModel.findOne({email});
     if(!userExist) return next(APIError.notFound("Account does not exist"));
-    if( !compareSync(userExist.password, password)) return next(APIError.badRequest("Incorrect password"));
-    
-  
+    if( !compareSync(password, userExist.password)) return next(APIError.badRequest("Incorrect password"));
+    const accessToken = jwt.sign({id:userExist._id, email:userExist.email}, process.env.ACCESS_TOKEN_SECRETE, {expiresIn: "30s"});
+    const refreshToken = jwt.sign({id:userExist._id, email: userExist.email}, process.env.REFRESH_TOKEN_SECRETE, {expiresIn: "5m"})
+  userExist.refreshToken = refreshToken;
+  userExist.save();
+  res.clearCookie("note_ap");
+  res.cookie("note_ap", accessToken, {
+    httpOnly: false,
+    secure: false,
+    sameSite: "none",
+  })
+  res.status(200).json({message: "Login successful", accessToken, refreshToken})
   } catch (error) {
     next(error)
   }
